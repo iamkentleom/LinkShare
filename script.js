80236e80212574a751605d42497b2baabb10ed8c
iamkentleom <leomakibulan@gmail.com>
2019-04-19 23:42:40 +0800

    Initial MVP

---
+++ script.js
@@ -0,0 +1,12 @@
+chrome.tabs.query({"active":true}, (tabs)=>{
+    const url = tabs[0].url
+    document.getElementById('link').innerText = url
+    new QRCode(document.getElementById("qrcode"), {
+        text: url,
+        width: 150,
+        height: 150,
+        colorDark : "#1d58ac",
+        colorLight : "#ffffff",
+        correctLevel : QRCode.CorrectLevel.H
+    })
+})
