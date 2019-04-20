f88e8acd52288107505201323db8dc01d4cf37fd
iamkentleom <leomakibulan@gmail.com>
2019-04-20 20:29:23 +0800

    Shorten url feature

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var xhttp = new XMLHttpRequest()
 chrome.tabs.query({"active":true}, (tabs)=>{
     const url = tabs[0].url
     document.getElementById('link').innerHTML = url
@@ -8,5 +9,30 @@ chrome.tabs.query({"active":true}, (tabs)=>{
         colorDark : "#1d4999",
         colorLight : "#ffffff",
         correctLevel : QRCode.CorrectLevel.H
+    })
+    document.getElementById('short').addEventListener('click', () => {
+        xhttp.onreadystatechange = () => {
+            console.log(xhttp)
+            if (xhttp.readyState == 4 && xhttp.status == 200) {
+                document.getElementById("qrcode").innerHTML = ''
+                let urlObject = JSON.parse(xhttp.responseText)
+                urlObject = urlObject["shorturl"]
+                document.getElementById('link').innerHTML = urlObject
+                document.getElementById('short').innerHTML = `Shorten Link`
+                new QRCode(document.getElementById("qrcode"), {
+                    text: urlObject,
+                    width: 250,
+                    height: 250,
+                    colorDark : "#1d4999",
+                    colorLight : "#ffffff",
+                    correctLevel : QRCode.CorrectLevel.H
+                })
+            }else{
+                document.getElementById('short').innerHTML = `Error. Retry?`
+            }
+        }
+        var tempURL = `https://is.gd/create.php?format=json&url=${encodeURIComponent(url)}`
+        xhttp.open("GET", tempURL)
+        xhttp.send()
     })
 })
