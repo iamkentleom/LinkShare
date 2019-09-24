65bdb0ed4327129a65eab12914765d990dbd448b
iamkentleom <leomakibulan@gmail.com>
2019-09-24 20:15:45 +0800

    Switch to fetch() api

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var xhttp = new XMLHttpRequest()
-chrome.tabs.query({"active":true}, (tabs)=>{
+chrome.tabs.query({"active":true}, (tabs) => {
     const url = tabs[0].url
     document.getElementById('link').innerText = url
     new QRCode(document.getElementById("qrcode"), {
@@ -11,29 +10,24 @@ chrome.tabs.query({"active":true}, (tabs)=>{
         correctLevel : QRCode.CorrectLevel.H
     })
     document.getElementById('short').addEventListener('click', () => {
-        xhttp.onreadystatechange = () => {
-            if (xhttp.readyState == 4 && xhttp.status == 200) {
-                document.getElementById("qrcode").innerHTML = ''
-                let urlObject = JSON.parse(xhttp.responseText)
-                urlObject = urlObject["shorturl"]
-                document.getElementById('link').innerText = urlObject
-                document.getElementById('short').innerText = `Shorten Link`
+        document.getElementById('short').innerText = `Loading...`
+        fetch(`https://is.gd/create.php?format=json&url=${encodeURIComponent(url)}`)
+            .then(res => res.json())
+            .then(json => {
+                document.getElementById('link').innerText = json.shorturl
+                document.getElementById('qrcode').innerHTML = ''
                 new QRCode(document.getElementById("qrcode"), {
-                    text: urlObject,
+                    text: json.shorturl,
                     width: 250,
                     height: 250,
                     colorDark : "#1d4999",
                     colorLight : "#ffffff",
                     correctLevel : QRCode.CorrectLevel.H
                 })
-            }else if(xhttp.readyState < 4){
-                document.getElementById('short').innerText = `Loading...`
-            }else{
+                document.getElementById('short').innerText = `Shorten Link`
+            })
+            .catch(() => {
                 document.getElementById('short').innerText = `Error. Retry?`
-            }
-        }
-        var tempURL = `https://is.gd/create.php?format=json&url=${encodeURIComponent(url)}`
-        xhttp.open("GET", tempURL)
-        xhttp.send()
+            })
     })
 })
